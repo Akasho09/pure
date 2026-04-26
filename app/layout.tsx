cf1a90f8c0b11bdbb48c55cb4d7d095a43c2b8d8
Akash Ahmad  Malik <aakashbwd@gmail.com>
2026-04-26 21:32:50 +0530

    f

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,7 +76,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=0.7, maximum-scale=0.7"
+        />
+      </head>
+
       <body>
         <AuthProvider>
           <AppShell>{children}</AppShell>
